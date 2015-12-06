c76ec51812ac0a3dbbfbc142534cd9898fe252d4
Erickson <ericksonsuarezocumarez@gmail.com>
2015-12-06 14:03:03 -0400

    Add figaron gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,6 +1,10 @@
 class Listing < ActiveRecord::Base
 
-	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "not_available.jpg"
-				 
-	validates_attachment_content_type :image, :content_type => %w(image/jpeg image/jpg image/png)
+	has_attached_file :image, :styles => { :medium => "200x", :thumb => "100x100>" }, :default_url => "not_available.jpg",
+					  :storage => :dropbox,
+				      :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+
+	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+				    		  
+
 end
