045b4ad3de9f7be6185d20514efefc4f34b7fac1
Erickson <ericksonsuarezocumarez@gmail.com>
2015-12-10 22:32:52 -0400

    Add validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -8,5 +8,8 @@ class Listing < ActiveRecord::Base
 				      :path => ":style/:id_:filename"
 
 	validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
-	end			    		  
+	end			
+	validates :name, :description, :price, presence: true
+	validates :price, numericality: {greater_than: 0}  
+	validates :image, attachment_presence: true  		  
 end
